5831eaba0a94c9fcf487d2f2f80f39ec4dc8b729
tfabiha <tfabiha@stuy.edu>
2019-01-30 20:02:32 -0500

    finished clear and drawing

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -2,22 +2,24 @@ var toggleb = document.getElementById("toggle");
 var clearb = document.getElementById("clear");
 
 var c = document.getElementById("slate");
+var canvas = c.getBoundingClientRect();
+
+var ctx = c.getContext("2d");
 
 toggleb.addEventListener("click", function(e)
 			    {
 				toggle();
 			    });
 
-clearb.addEventListener("clear", function(e)
-			  {
-			      clear();
-			  });
-
-var ctx = c.getContext("2d");
+clearb.addEventListener("click", function(e)
+			{
+			    console.log("clearing");
+			    clear();
+			});
 
 c.addEventListener("click", function(e)
 		     {
-			 draw();
+			 draw(e);
 		     });
 
 var toggle = function(e)
@@ -27,10 +29,19 @@ var toggle = function(e)
 
 var clear = function(e)
 {
-
+    ctx.clearRect(0, 0, c.width, c.height);;
 };
 
 var draw = function(e)
 {
+    console.log("(" + e.clientX + ", " + e.clientY + ")");
 
+    var x = e.clientX - canvas.left;
+    var y = e.clientY - canvas.top;
+
+    console.log(x + " " + y);
+
+    ctx.fillStyle = "#ff0000";
+    ctx.fillRect(x, y, 20, 20);
 };
+
