499008c58b027bee4c60e5991f1d8c4dfd1b1e29
tfabiha <tfabiha@stuy.edu>
2019-03-04 23:35:31 -0500

    done

--- 07_mongo/mango.py
+++ 07_mongo/mango.py
@@ -3,6 +3,20 @@
 #K07 -- Import/Export Bank
 #2019-03-05
 
+"""
+The data set we have used is movies and it contains:
+- titles of movies
+	- year
+	- cast 
+	- genres 
+
+This dataset is hosted https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json
+
+Our import mechanism is to get the json file and read the file. We then use 
+the json.load() in order to decode the data and then insert that into the db 
+collection we have initialized.
+"""
+
 import json
 import pymongo
 
@@ -30,21 +44,20 @@ def with_actor(actor):
         print(i)
 
 def of_genre(genre):
-    obj = collection.find({"genre" : genre})
+    obj = collection.find({"genres" : genre})
 
     for i in obj:
         print(i)
 
 def with_actors(actor0, actor1):
-    obj = collection.find({"$and" : [{"actor" : actor0}, {"actor" : actor1}] })
+    obj = collection.find({"$and" : [{"cast" : actor0}, {"cast" : actor1}] })
 
     for i in obj:
         print(i)
 
 
-#in_borough("Queens")
-#in_zip("11432")
-#in_zip_w_grade("11432", "Z")
-#in_zip_below("11432" , 50)
 create("movies.json")
-in_year(1900)
+#in_year(1900)
+#with_actor("Bradley Cooper")
+#of_genre("Crime")
+with_actors("Bradley Cooper", "Clint Eastwood")
