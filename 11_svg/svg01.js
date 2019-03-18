e8e6dd373c72eebe550a071127bd9bd2ec6a569c
tfabiha <tfabiha@stuy.edu>
2019-03-17 23:36:01 -0400

    done :)

--- 11_svg/svg01.js
+++ 11_svg/svg01.js
@@ -1,10 +1,17 @@
 // T Fabiha
 // SoftDev pd7
-// K10 -- Ask Circles [Change || Die]
-// 2019-03-14r
+// K #11: Ask Circles [Change || Die] …While On The Go
+// 2019-03-18m
+
+
+var HEIGHT = 500;
+var WIDTH = 500;
+var requestID;
 
 var pic = document.getElementById("vimage");
 var clear = document.getElementById("but_clear");
+var move = document.getElementById("moving");
+var question = document.getElementById("question");
 
 pic.addEventListener( "click", function(e)
 		      {
@@ -17,6 +24,18 @@ clear.addEventListener( "click", function(e)
 			    clearing(e);
 			}
 		      );
+
+move.addEventListener("click", function(e)
+		      {
+			  circleMove(e);
+		      }
+		     );
+
+question.addEventListener("click", function(e)
+			  {
+			      randomVel(e);
+			  }
+			 );
 
 var draw = true;
 
@@ -48,6 +67,8 @@ var circle = function( c, x, y )
     c.setAttribute( "stroke", "black" );
     c.setAttribute( "cx", x );
     c.setAttribute( "cy", y );
+    c.setAttribute( "xVel", 1 );
+    c.setAttribute( "yVel", 1 );
     
     pic.appendChild( c );
 }
@@ -91,4 +112,56 @@ var childclicked = function(e)
     }
 };
 
+var circleMove = function(e)
+{
+    window.cancelAnimationFrame(requestID);
+    
+    var children = pic.children;
+    
+    for (var i = 0; i < children.length; i++)
+    {
+	window.cancelAnimationFrame(requestID);
+	
+	var xVel = parseInt( children[i].getAttribute("xVel") );
+	var yVel = parseInt( children[i].getAttribute("yVel") );
+	
+	var x = parseInt( children[i].getAttribute("cx") );
+	var y = parseInt( children[i].getAttribute("cy") );
 
+	console.log(x + " " + y);
+	
+	if (x >= WIDTH || x <= 0)
+	{
+	    xVel *= -1;
+	    children[i].setAttribute( "xVel",  xVel );
+	}
+	if (y >= HEIGHT || y <= 0)
+	{
+	    yVel *= -1;
+	    children[i].setAttribute( "yVel",  yVel );
+	}
+
+	x += xVel;
+	y += yVel;
+	
+	children[i].setAttribute( "cx", x );
+	children[i].setAttribute( "cy", y );
+
+	requestID = requestAnimationFrame(circleMove);
+    }
+    
+};
+
+var randomVel = function(e)
+{
+    children = pic.children;
+
+    for (var i = 0; i < children.length; i++)
+    {
+	var xVel = Math.floor( Math.random() * 10 );
+	var yVel = Math.floor( Math.random() * 10 );
+
+	children[i].setAttribute( "xVel", xVel );
+	children[i].setAttribute( "yVel", yVel );
+    }
+}
