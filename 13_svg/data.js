5fc87027bf0e208733b0029584bc934f047c9228
tfabiha <tfabiha@stuy.edu>
2019-03-21 13:01:20 -0400

    it works????

--- 13_svg/data.js
+++ 13_svg/data.js
@@ -8,27 +8,28 @@ var chart = d3.select(".chart")
     .attr("width", width);
 
 d3.tsv("data.tsv", type, function(error, data) {
-  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+    console.log(data)
+    x.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-  chart.attr("height", barHeight * data.length);
+    chart.attr("height", barHeight * data.length);
 
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    var bar = chart.selectAll("g")
+	.data(data)
+	.enter().append("g")
+	.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
-  bar.append("rect")
-      .attr("width", function(d) { return x(d.value); })
-      .attr("height", barHeight - 1);
+    bar.append("rect")
+	.attr("width", function(d) { return x(d.value); })
+	.attr("height", barHeight - 1);
 
-  bar.append("text")
-      .attr("x", function(d) { return x(d.value) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.value; });
+    bar.append("text")
+	.attr("x", function(d) { return x(d.value) - 3; })
+	.attr("y", barHeight / 2)
+	.attr("dy", ".35em")
+	.text(function(d) { return d.value; });
 });
 
 function type(d) {
-  d.value = +d.value; // coerce to number
-  return d;
+    d.value = +d.value; // coerce to number
+    return d;
 }
