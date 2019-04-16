bb723ebe8d35479484e0d0573e9e8ee44b8bc6fe
tfabiha <tfabiha@stuy.edu>
2019-04-16 14:59:57 -0400

    ......

--- 18_listcomp/listcomp.py
+++ 18_listcomp/listcomp.py
@@ -1,7 +1,12 @@
-# T Fabiha
+# Emily Lee && T Fabiha
 # SoftDev2 pd7
 # K#18
 # 2019-04-16
+
+def pythag(n):
+    return [[x,y,int((x**2+y**2)**0.5)] for x in range(1,n+1) for y in range(1,n+1) if ((x**2+y**2)**0.5).is_integer() and x<=y]
+
+print(pythag(19))
 
 def quicksort(li, min_place, max_place):
 
