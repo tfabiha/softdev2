91807d50e170ec5494b102db0020a4ad391bcbed
tfabiha <tfabiha@stuy.edu>
2019-04-29 13:21:10 -0400

    its broken

--- 21_js-mfr/education.js
+++ 21_js-mfr/education.js
@@ -1,3 +1,22 @@
-# T Fabiha
+// T Fabiha
 
-data = 1
+var data;
+
+d3.queue()
+    .defer(d3.json, "data.json")
+    .await(ready);
+
+function ready( edu )
+{
+    edu.forEach(  )
+};
+
+console.log(data);
+
+var x = [1, 2, 3, 4]
+var newX = x.map( function(n)
+		  {
+		      return (n * 2)
+		  });
+
+console.log( newX );
