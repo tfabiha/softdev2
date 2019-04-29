58c07f6307b912ab4062ba1077dba0ea4cf9b6fe
tfabiha <tfabiha@stuy.edu>
2019-04-29 09:45:04 -0400

    ...

--- 20_anon-reduce/listcomp.py
+++ 20_anon-reduce/listcomp.py
@@ -1,5 +1,9 @@
 # Team SkaterSquad - Emily Lee && T Fabiha
+# SoftDev2 pd7
+# K20: Reductio ad Absurdum
+# 2019-04-29
 
+import datetime
 from functools import reduce
 
 #read Grimm's Fairy Tails
@@ -39,8 +43,19 @@ def find_mult( li ):
 print( "count fox to: {}".format( find_mult( ["fox", "to"] ) ) )
 
 # Find the most frequently occurring word
-def findmost():
-    counts = [ ( x, find_1(x) ) for x in words ]
 
-    return reduce( (lambda x, y: x if x[1] > y[1] else y), counts )
-print( findmost() )
+def findmost():
+    print(datetime.datetime.now())
+    
+    allw=[]
+    [allw.append(x) for x in words if x not in allw]
+    
+    print(datetime.datetime.now())
+    
+    counts = [ [ x, find_1(x) ] for x in allw ]
+
+    print(datetime.datetime.now())
+
+    return reduce( (lambda x, y: x if x[1] >= y[1] else y), counts )
+
+    print( findmost() )
