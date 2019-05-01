7b0cd7cf16e205ae91c1115a563b4a98c3420e5d
tfabiha <tfabiha@stuy.edu>
2019-05-01 13:21:17 -0400

    sgfsf

---
+++ 23_sdsd/closure.py
@@ -0,0 +1,39 @@
+import random
+
+def make_HTML_heading(f):
+    txt = f()
+    def inner():
+        return "<h1>" + txt + "</h1>"
+    return inner
+
+# equiv to greet = makeHTMLheading(greet)
+@make_HTML_heading
+def greet():
+    greetings = ["Hello", "Welcome", "AYO!", "Hola", "Bonjour", "Wordup"]
+    return random.choice(greetings)
+
+# greet_heading = make_HTML_heading( greet )
+# print( greet_heading() )
+
+print( greet() )
+print( greet() )
+
+def memoize(f):
+    memo = {}
+    def helper(x):
+        nonlocal memo
+
+        if memo[x-1] 
+        
+    return helper
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+fib = memoize(fib)
+print( fib(40) )
